4bed86f793c53e0044ce9a08d0070be147aa07a7
Rohit Shah <143074565+rnr-roah@users.noreply.github.com>
2024-08-09 21:51:57 +0545

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+	    document.getElementById('menu-toggle').addEventListener('click', function() {
+		            var projectsSection = document.getElementById('projects-section');
+		            projectsSection.classList.toggle('open');
+		        });
+});
+
