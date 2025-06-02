b4d10785389c9a6dfd386056cae5a79c2eeeba08
Rohit Shah <143074565+rnr-roah@users.noreply.github.com>
2025-06-02 15:08:01 +0000

    blog page finalized

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-<h1>Website up at <a href="https://portfolio-rohitshah.web.app">Portfolio-Website</a>
-</h1>
+#Website up at <a href="https://portfolio-rohitshah.web.app">Portfolio-Website</a>
--- public/flashcards.js
+++ public/flashcards.js
@@ -1,11 +1,11 @@
 
-document.querySelectorAll('.profile-card').forEach(card => {
-    // Add a click event listener to each card
-    card.addEventListener('click', () => {
-        // Toggle the 'flipped' class on click
-        card.classList.toggle('swapped');
-    });
-});
+// document.querySelectorAll('.profile-card').forEach(card => {
+//     // Add a click event listener to each card
+//     card.addEventListener('click', () => {
+//         // Toggle the 'flipped' class on click
+//         card.classList.toggle('swapped');
+//     });
+// });
 
 
 document.querySelectorAll('.flashcard1').forEach(card => {
@@ -17,11 +17,11 @@ document.querySelectorAll('.flashcard1').forEach(card => {
 });
 
 
-document.querySelectorAll('.gallerycard').forEach(card => {
-    // Add a click event listener to each card
-    card.addEventListener('click', () => {
-        // Toggle the 'flipped' class on click
-        card.classList.toggle('flipped');
-    });
-});
+// document.querySelectorAll('.gallerycard').forEach(card => {
+//     // Add a click event listener to each card
+//     card.addEventListener('click', () => {
+//         // Toggle the 'flipped' class on click
+//         card.classList.toggle('flipped');
+//     });
+// });
 
